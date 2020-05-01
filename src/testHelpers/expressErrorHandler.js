eb445ddc2e241000df11b4e76384ccff41c49d81
Zoha Banam <Zoha.Banam@gmail.com>
2020-05-01 23:48:17 +0430

    add eslint test

--- src/testHelpers/expressErrorHandler.js
+++ src/testHelpers/expressErrorHandler.js
@@ -1,4 +1,4 @@
-module.exports = (error, req, res, next) => {
+module.exports = (error, req, res) => {
   console.log(error)
   return res.status(500).json(error)
 }
---
+++ tests/lint.js
@@ -0,0 +1,32 @@
+const glob = require("glob")
+const { CLIEngine } = require("eslint")
+const path = require("path")
+
+describe("ESLint", function() {
+  this.timeout(8000)
+  it("test code style by lint", () => {
+    const paths = glob.sync(path.join(__dirname, "../src/") + "**/*.js")
+    const engine = new CLIEngine({
+      envs: ["node", "mocha"],
+      useEslintrc: true
+    })
+    const results = engine.executeOnFiles(paths).results
+
+    const finalMessages = []
+    results.forEach(result => {
+      const { messages, filePath } = result
+      if (messages.length > 0) {
+        messages.forEach(message => {
+          finalMessages.push(
+            `${filePath}:${message.line}:${message.column} ${message.message.slice(0, -1)} - ${
+              message.ruleId
+            }\n`
+          )
+        })
+      }
+    })
+    if (finalMessages.length > 0) {
+      throw new Error("there was errors \n" + finalMessages.join("\n"))
+    }
+  })
+})
