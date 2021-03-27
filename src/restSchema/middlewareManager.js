d9a840be4ce2047df9ecf92e75e5abfbe1e37c0a
Zoha Banam <Zoha.Banam@gmail.com>
2021-03-27 09:12:56 +0430

    add middlewareManager

---
+++ src/restSchema/middlewareManager.js
@@ -0,0 +1,106 @@
+const defaultSchemaRoutes = require("./defaults/schema/defaultSchemaRoutes")
+
+/**
+ * @typedef {import("../../typeDefs/schema").middleware} middleware
+ */
+
+class MiddlewareManager {
+  constructor() {
+    this.defaultRoutes = Object.keys(defaultSchemaRoutes)
+    this.middlewareList = {}
+    this.setDefaultMiddlewareList()
+  }
+
+  /**
+   * @private
+   */
+  setDefaultMiddlewareList() {
+    const middlewareList = {
+      global: []
+    }
+    for (const routeName of this.defaultRoutes) {
+      middlewareList[routeName] = []
+    }
+    this.middlewareList = middlewareList
+  }
+
+  /**
+   *
+   * @param {(Array<string>| string)} route
+   * @param {middleware | middleware[]} middleware
+   * @return {MiddlewareManager}
+   */
+  addToRoute(route, middleware) {
+    if (Array.isArray(route)) {
+      for (const routeItem of route) {
+        this.addToRoute(routeItem, middleware)
+      }
+    } else {
+      route = route.toLowerCase()
+      if (!this.middlewareList[route]) {
+        this.middlewareList[route] = []
+      }
+      if (!Array.isArray(middleware)) {
+        middleware = [middleware]
+      }
+      for (const item of middleware) {
+        this.middlewareList[route].push(item)
+      }
+    }
+    return this
+  }
+
+  /**
+   * @param {middleware | middleware[]} middleware
+   * @return {MiddlewareManager}
+   */
+  addToGlobal(middleware) {
+    this.addToRoute("global", middleware)
+    return this
+  }
+
+  /**
+   * @param {middleware | middleware[]} middleware
+   * @param {string | Array<string>} exceptRoutes
+   * @return {MiddlewareManager}
+   */
+  add(middleware, exceptRoutes = []) {
+    if (!Array.isArray(exceptRoutes)) {
+      exceptRoutes = [exceptRoutes]
+    }
+    const targetRoutes = Object.keys(this.middlewareList).filter(
+      i => !exceptRoutes.includes(i) || i === "global"
+    )
+    this.addToRoute(targetRoutes, middleware)
+    return this
+  }
+
+  /**
+   * @param {string | Array<string>} exceptRoutes
+   * @param {middleware | middleware[]} middleware
+   * @return {MiddlewareManager}
+   */
+  addToAllRoutesExcept(exceptRoutes, middleware) {
+    this.add(middleware, exceptRoutes)
+    return this
+  }
+
+  /**
+   * @param {string | Array<string>} targetRoutes
+   * @param {middleware | middleware[]} middleware
+   * @return {MiddlewareManager}
+   */
+  addToThisRoutes(targetRoutes, middleware) {
+    this.addToRoute(targetRoutes, middleware)
+    return this
+  }
+
+  /**
+   * @returns {middleware}
+   */
+  get() {
+    return this.middlewareList
+  }
+}
+
+module.exports = MiddlewareManager
--- src/restSchema/routeManager.js
+++ src/restSchema/routeManager.js
@@ -5,54 +5,64 @@
 
 class RouteManager {
   /**
-   * @param {route[]} routes
+   * @param {routes} routes
    */
   constructor(routes) {
     this.routes = Object.values(routes)
   }
 
   /**
-   * @param {route} newRoute
+   * @param {route | route[]} newRoute
    * @return {RouteManager}
    */
   add(newRoute) {
-    this.routes = [...this.routes, newRoute]
+    if (Array.isArray(newRoute)) {
+      for (const route of newRoute) {
+        this.add(route)
+      }
+    } else {
+      this.routes = [...this.routes, newRoute]
+    }
     return this
   }
 
   /**
-   * @param {route} newRoute
+   * @param {route | route[]} newRoute
    * @return {RouteManager}
    */
   addRoute(newRoute) {
-    this.routes = [...this.routes, newRoute]
-    return this
+    return this.add(newRoute)
   }
 
   /**
-   * @param {route[]} newRoutes
+   * @param {route | route[]} newRoutes
    * @return {RouteManager}
    */
   addRoutes(newRoutes) {
-    this.routes = [...this.routes, ...newRoutes]
-    return this
+    return this.add(newRoutes)
   }
 
   /**
-   * @param {route[]} routes
+   * @param {route[]|route} routes
    * @return {RouteManager}
    */
   only(routes) {
+    if (!Array.isArray(routes)) {
+      routes = [routes]
+    }
     // @ts-ignore
     this.routes = this.routes.filter(i => routes.includes(i) || routes.includes(i.name))
     return this
   }
 
   /**
-   * @param {route[]} routes
+   * @param {route[]|route} routes
    * @return {RouteManager}
    */
   except(routes) {
+    if (!Array.isArray(routes)) {
+      routes = [routes]
+    }
     // @ts-ignore
     this.routes = this.routes.filter(i => !routes.includes(i) && !routes.includes(i.name))
 
