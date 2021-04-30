04e0e3a20d12780d501b63931593ffb194bbeb39
Zoha Banam <Zoha.Banam@gmail.com>
2021-05-01 00:31:24 +0430

    add route middleware test

---
+++ tests/usage/routeProps.test.js
@@ -0,0 +1,81 @@
+/* eslint-disable no-underscore-dangle */
+const { expect } = require("chai")
+const express = require("express")
+const request = require("supertest")
+const bodyParser = require("body-parser")
+const model = require("../../src/testHelpers/model")
+const expressErrorHandler = require("../../src/testHelpers/expressErrorHandler")
+const { Mixed } = require("../../src/restSchema/types")
+const { schema: createSchema } = require("../../src/restSchema/index")
+
+const app = express()
+
+const schema = {
+  model,
+  fields: {
+    prop1: String
+  }
+}
+
+describe("route props", () => {
+  beforeEach(async () => {
+    await model.deleteMany()
+    app.use(bodyParser.json())
+    app.use(expressErrorHandler)
+  })
+
+  it("works with single middleware", async () => {
+    const ModelSchema = createSchema(model, schema.fields, {
+      ...schema,
+      routes: [
+        {
+          name: "test",
+          path: "/test",
+          middleware: (req, res, next) => {
+            res.status(440) // a custom error
+            return next(new Error())
+          },
+          handler() {
+            return {
+              message: true
+            }
+          }
+        }
+      ]
+    })
+    app.use("/test1", ModelSchema.resource(schema))
+
+    request(app)
+      .get("/test1")
+      .expect(440)
+  })
+
+  it("works with multiple middleware", async () => {
+    const ModelSchema = createSchema(model, schema.fields, {
+      ...schema,
+      routes: [
+        {
+          name: "test",
+          path: "/test",
+          middleware: [
+            (req, res, next) => next(),
+            (req, res, next) => {
+              res.status(450) // a custom error
+              return next(new Error())
+            }
+          ],
+          handler() {
+            return {
+              message: true
+            }
+          }
+        }
+      ]
+    })
+    app.use("/test2", ModelSchema.resource(schema))
+
+    request(app)
+      .get("/test2")
+      .expect(450)
+  })
+})
